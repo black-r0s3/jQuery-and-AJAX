c686ca1e70eb72919b09da387af46c8ded9f0b97
black-r0s3 <ebenezer.nyarko@amalitech.org>
2020-10-11 00:16:55 +0800

    loaded items from json file

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
-  $(document).ready(function() {
-  $('#button-create-item').on('click', function () {
-    let name = $('#input-create-item').val();
-    $('#input-create-item').val('');
-
+  function addItem(name, description, price, moreInfo) {
     let html = '';
-    html+='<div class="item">'
-    html+='<div class="name">' + name +   '  </div>'
-    html+='<img src="assets/beach.jpg">'
-    html+='<div class="description">'
+  html += '<div class="item">';
+  html += '<div class="name">' + name + '</div>';
+  html += '<img src="assets/beach.jpg"></img>';
+  html += '<div class="description">' + description + '</div>';
+  html += '<div class="price">' + price + '</div>';
+  html += '<button class="item-add">Add to cart</button>';
+  html += '<button class="item-remove">Remove</button>';
+  html += '<br/>';
+  html += '<a class="more-info-link" href="#">More info</a>';
+  html += '<div class="more-info">' + moreInfo + '</div>';
+  html += '</div>';
 
-    html+='</div>'
-    html+='<div class="price">499</div>'
-    html+='<button type="button" name="button" class="item-add">Add to cart</button>'
-    html+='<button type="button" name="button" class="item-remove">Remove</button>'
-    html+='<br>'
-    html+='<a class="more-info-link" href="#">More Info</a>'
-    html+='<div class="more-info">Lorem ipsumm dlor sitt  </div>'
-
-    html+='</div>'
-    $('#container').prepend(html);
-
-  });
+  $('#container').prepend(html);
+  }
+  $(document).ready(function() {
     $('#container').on('click', '.more-info-link', function(event) {
       event.preventDefault();
       $(this).parent().find('.more-info').slideToggle('slow');
@@ -28,10 +22,28 @@
         .animate({"opacity": 0.5,
         "margin-left":10 } , 150)
         .animate({"opacity": 1.0, "margin-left":10 } , 150);
-
     });
     $('#container').on('click','.item-remove', function() {
       $(this).parent().remove();
-
     });
+    $.ajax('data/item.json', {
+      dataType: 'json',
+      contentType: 'application/json',
+      cache: false
+      
+    })
+      .done(function(response) {
+        let items = response.items;
+        items.forEach(function(item) {
+            addItem(item.name, item.description, item.price, item.moreInfo);
+        });
+
+      })
+      .fail(function(request, errorType, errorMessage) {
+          console.log(errorMessage);
+      })
+      .always(function () {
+
+      })
+
 });
