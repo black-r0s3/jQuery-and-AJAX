0860d925c1df80b26ae56018de57a7129664e6f4
black-r0s3 <ebenezer.nyarko@amalitech.org>
2020-10-11 01:13:09 +0800

    added post calls using ajax

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-  function addItem(name, description, price, moreInfo) {
+var cart = 0;
+function addItem(id, name, description, price, moreInfo) {
     let html = '';
-  html += '<div class="item">';
+  html += '<div class="item" data-id="' + id + '">';
   html += '<div class="name">' + name + '</div>';
   html += '<img src="assets/beach.jpg"></img>';
   html += '<div class="description">' + description + '</div>';
@@ -30,12 +31,12 @@
       dataType: 'json',
       contentType: 'application/json',
       cache: false
-      
+
     })
       .done(function(response) {
         let items = response.items;
         items.forEach(function(item) {
-            addItem(item.name, item.description, item.price, item.moreInfo);
+            addItem(item.id, item.name, item.description, item.price, item.moreInfo);
         });
 
       })
@@ -44,6 +45,23 @@
       })
       .always(function () {
 
-      })
+      });
+      $('#container').on('click', '.item-add', function() {
+        let id = $(this).parent().data('id');
+        $.ajax('data/addToCart.json',{
+          data: {id: id },
+          type: 'post',
+          dataType: 'json',
+          contentType: 'application/json'
+        })
+        .done(function(response) {
+      if (response.message === 'success') {
+        let price = response.price;
 
+        cart += price;
+
+        $('#cart-container').text('$' + cart);
+      }
+      });
+      });
 });
